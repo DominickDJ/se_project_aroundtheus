cb10947a60516771ef99afc47d45e6d6062e7730
Dominick Harper <dominickdustinjames@gmail.com>
2023-03-29 22:39:32 -0600

    feat:valitation

--- scripts/validation.js
+++ scripts/validation.js
@@ -7,22 +7,28 @@ const config = {
   errorClass: "modal__error_visible",
 };
 
-function showInputError(formElement, inputElement, [inputErrorClass]) {
-  const { inputErrorClass } = options;
+function showInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
   const errorMessageElement = formElement.querySelector(
     "#" + inputElement.id + "-error"
   );
-  inputElement.classList.add(inputErrorClass, errorClass);
+  inputElement.classList.add(inputErrorClass);
   errorMessageElement.textContent = inputElement.validationMessage;
   errorMessageElement.classList.add(errorClass);
 }
 
-function hideInputError(formElement, inputElement, [inputErrorClass]) {
-  const { inputErrorClass } = options;
+function hideInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
   const errorMessageElement = formElement.querySelector(
     "#" + inputElement.id + "-error"
   );
-  inputElement.classList.remove(inputErrorClass, errorClass);
+  inputElement.classList.remove(inputErrorClass);
   errorMessageElement.textContent = "";
   errorMessageElement.classList.remove(errorClass);
 }
@@ -38,7 +44,11 @@ function hasInvalidInput(inputList) {
   return !inputList.every((inputElement) => inputElement.validity.valid);
 }
 
-function toggleButtonState(formElement, submitButton, { inactiveButtonClass }) {
+function toggleButtonState(
+  inputElements,
+  submitButton,
+  { inactiveButtonClass }
+) {
   if (hasInvalidInput(inputElements)) {
     submitButton.classList.add(inactiveButtonClass);
     submitButton.disabled = true;
